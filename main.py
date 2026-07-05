fc3cc8e4b1ce87a5c068247cd2432d533df97757
machingclee <machingclee@gmail.com>
2026-07-06 05:04:14 +0800

    update python version

--- main.py
+++
@@ -1,8 +0,0 @@
-# Local development entry point. NOT used on Lambda.
-# Run with:  python main.py   (http://127.0.0.1:8000)
-from app import app
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app:app", host="127.0.0.1", port=3030, reload=True)
